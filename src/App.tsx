dea84d9f6112b9c98f42914fe524ed6e67bf1271
Adrián Romero <adrianromero@users.noreply.github.com>
2023-08-03 23:50:31 +0200

    Adding antd buttons and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,34 @@
 import React from 'react';
+import type { FC } from 'react';
 import { invoke } from "@tauri-apps/api/tauri";
-import logo from "./logo.svg";
+import { Button, Space, Tag } from 'antd';
+
 import "./App.css";
 
-function App() {
-  const doClick = () => {
-    invoke("my_function");
-    invoke("my_other_function").then(() => invoke("my_function"));
-  };
-  const doStart = () => {
-    invoke("start_server").then(() => alert("started"));
-  };
-  const doStop = () => {
-    invoke("stop_server").then(() => alert("started"));
-  };
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={doClick}>Press me now</button>
-        <button onClick={doStart}>Start Server</button>
-        <button onClick={doStop}>Stop Server</button>
-      </header>
-    </div>
-  );
+const App: FC = () => {
+    const doClick = () => {
+        invoke("my_function");
+        invoke("my_other_function").then(() => invoke("my_function"));
+    };
+    const doStart = () => {
+        invoke("start_server").then(() => alert("started"));
+    };
+    const doStop = () => {
+        invoke("stop_server").then(() => alert("started"));
+    };
+    return (
+        <div className="App">
+
+
+            <Space>
+                <Button type="primary" onClick={doClick}>Press me now</Button>
+                <Button onClick={doStart}>Start Server</Button>
+                <Button onClick={doStop}>Stop Server</Button>
+                <Tag color="magenta">magenta</Tag>
+            </Space>
+
+        </div>
+    );
 }
 
 export default App;
