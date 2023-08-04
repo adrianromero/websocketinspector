55d151aa321dec5daf825dcac0cfaeca7f2ae642
Adrián Romero <adrianromero@users.noreply.github.com>
2023-08-04 20:58:30 +0200

    Render empty and blank messages

--- src/LoggingEvents.tsx
+++ src/LoggingEvents.tsx
@@ -24,11 +24,93 @@ import CallMadeIcon from '@mui/icons-material/CallMade';
 import { green, red, blue } from '@mui/material/colors';
 import type { LogEvent } from "./features/websocketSlice"
 
+
+type SecondaryItemProps = {
+    paragraph: string;
+};
+const SecondaryItem: FC<SecondaryItemProps> = ({ paragraph }: SecondaryItemProps) => {
+    if (paragraph) {
+        console.log(paragraph.trim().length)
+        if (paragraph.trim()) {
+            return <Typography
+                sx={{
+                    whiteSpace: "nowrap",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis"
+                }}
+                variant="body2"
+                color="text.secondary"
+            >{paragraph}</Typography>;
+        }
+        return <Typography variant="body2" color="text.disabled">{"<blank>"}</Typography>;
+    }
+    return <Typography variant="body2" color="text.disabled">{"<empty>"}</Typography>;
+}
+
+type LoggingItemProps = {
+    logEvent: LogEvent;
+    displayaddress?: boolean;
+};
+const LoggingItem: FC<LoggingItemProps> = ({ logEvent, displayaddress }: LoggingItemProps) => {
+
+    const { kind, time, payload } = logEvent;
+    let label: string;
+    let icon: ReactNode;
+    let paragraph: string;
+    const address = displayaddress ? ` ${payload.client.address}` : "";
+
+    if (kind === "connect") {
+        label = "CONNECT" + address;
+        icon = <Avatar sx={{ bgcolor: green[500], height: 24, width: 24 }} ><LinkIcon sx={{ fontSize: 16 }} /></Avatar>;
+        paragraph = "/" + payload.tail;
+    } else if (kind === "disconnect") {
+        label = "DISCONNECT" + address;
+        icon = <Avatar sx={{ bgcolor: red[500], height: 24, width: 24 }} ><LinkOffIcon sx={{ fontSize: 16 }} /></Avatar>;
+        paragraph = payload.message
+            ? `${payload.message.code}: ${payload.message.reason}`
+            : "unknown:";
+
+    } else if (kind === "message") {
+        icon = payload.direction === "CLIENT"
+            ? <Avatar sx={{ bgcolor: blue[500], height: 24, width: 24 }} ><CallReceivedIcon sx={{ fontSize: 16 }} /></Avatar>
+            : <Avatar sx={{ bgcolor: blue[500], height: 24, width: 24 }} ><CallMadeIcon sx={{ fontSize: 16 }} /></Avatar>;
+        if ("TEXT" in payload.message) {
+            label = "TEXT" + address;
+            paragraph = payload.message.TEXT.msg;
+        } else if ("BINARY" in payload.message) {
+            label = "BINARY" + address;
+            paragraph = "BASE64"; // payload.message.BINARY.msg;
+        } else {
+            label = "UNKNOWN" + address;
+            paragraph = "";
+        }
+    } else {
+        label = "UNKNOWN";
+        icon = null;
+        paragraph = "";
+    }
+    return (<>
+        <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+                {icon}
+            </ListItemAvatar>
+            <ListItemText
+                primary={label}
+                secondary={<SecondaryItem paragraph={paragraph} />}
+            />
+            <Typography variant="body2" noWrap align="right" sx={{ minWidth: 200, color: 'text.secondary' }}>
+                {time.toLocaleString()}
+            </Typography>
+        </ListItem >
+        <Divider component="li" />
+    </>
+    );
+}
+
 export type LoggingEventsProps = {
     className?: string;
     clientlog: LogEvent[];
     displayaddress?: boolean;
-
 };
 const LoggingEvents: FC<LoggingEventsProps> = (props: LoggingEventsProps) => {
 
@@ -43,71 +125,7 @@ const LoggingEvents: FC<LoggingEventsProps> = (props: LoggingEventsProps) => {
     return (
         <div ref={listRef} className={className}>
             <List sx={{ bgcolor: 'background.paper' }} dense disablePadding>
-                {clientlog.map(item => {
-                    const { kind, time, payload } = item;
-                    let label: string;
-                    let icon: ReactNode;
-                    const address = displayaddress ? ` ${payload.client.address}` : "";
-
-                    let paragraph;
-                    if (kind === "connect") {
-                        label = "CONNECT" + address;
-                        icon = <Avatar sx={{ bgcolor: green[500], height: 24, width: 24 }} ><LinkIcon sx={{ fontSize: 16 }} /></Avatar>;
-                        paragraph = "/" + payload.tail;
-                    } else if (kind === "disconnect") {
-                        label = "DISCONNECT" + address;
-                        icon = <Avatar sx={{ bgcolor: red[500], height: 24, width: 24 }} ><LinkOffIcon sx={{ fontSize: 16 }} /></Avatar>;
-                        let text: string = payload.message
-                            ? `${payload.message.code}: ${payload.message.reason}`
-                            : "unknown:";
-                        paragraph = text;
-                    } else if (kind === "message") {
-                        let text: string;
-                        icon = payload.direction === "CLIENT"
-                            ? <Avatar sx={{ bgcolor: blue[500], height: 24, width: 24 }} ><CallReceivedIcon sx={{ fontSize: 16 }} /></Avatar>
-                            : <Avatar sx={{ bgcolor: blue[500], height: 24, width: 24 }} ><CallMadeIcon sx={{ fontSize: 16 }} /></Avatar>;
-                        if ("TEXT" in payload.message) {
-                            label = "TEXT" + address;
-                            text = payload.message.TEXT.msg;
-                        } else if ("BINARY" in payload.message) {
-                            label = "BINARY" + address;
-                            text = "BASE64"; // payload.message.BINARY.msg;
-                        } else {
-                            label = "UNKNOWN" + address;
-                            text = "";
-                        }
-                        paragraph = text
-                    } else {
-                        label = "UNKNOWN";
-                        icon = null;
-                        paragraph = null;
-                    }
-                    return (<>
-                        <ListItem alignItems="flex-start">
-                            <ListItemAvatar>
-                                {icon}
-                            </ListItemAvatar>
-                            <ListItemText
-                                primary={label}
-                                secondary={
-                                    <Typography
-                                        sx={{
-                                            whiteSpace: "nowrap",
-                                            overflow: "hidden",
-                                            textOverflow: "ellipsis"
-                                        }}
-                                        variant="body2"
-                                        color="text.secondary"
-                                    >{paragraph}</Typography>}
-                            />
-                            <Typography variant="body2" noWrap align="right" sx={{ minWidth: 200, color: 'text.secondary' }}>
-                                {time.toLocaleString()}
-                            </Typography>
-                        </ListItem >
-                        <Divider component="li" />
-                    </>
-                    );
-                })}
+                {clientlog.map(logEvent => <LoggingItem logEvent={logEvent} displayaddress={displayaddress} />)}
             </List >
         </div >
     );
